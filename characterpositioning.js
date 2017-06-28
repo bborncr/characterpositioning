dbcf8d1eb15261a5bc73423f81fe7959b3cf2a10
bborncr <bentley@crcibernetica.com>
2017-06-28 17:46:32 +0000

    it started working!

--- characterpositioning.js
+++ characterpositioning.js
@@ -9,12 +9,24 @@ function countLetters(inputText){
   // create the letterCount object
   var letterCount = {};
 
+  // var letterCount = {
+  //   a: {
+  //     count: 3,
+  //     positions: [3, 4, 1]
+  //   },
+  //   b: {
+  //     count: 1,
+  //     positions:[0]
+  //   }
+  // }
+
   // loop through and count the letters
   for (var i = 0; i < inputText.length; i++) {
     if (letterCount[inputText[i]] !== undefined){
-      letterCount[inputText[i]] = letterCount[inputText[i]] + 1;
+      letterCount[inputText[i]]['count'] = letterCount[inputText[i]]['count'] + 1;
+      letterCount[inputText[i]]['positions'].push(i);
     } else {
-      letterCount[inputText[i]] = 1;
+      letterCount[inputText[i]] = {'count': 1, 'positions': [i]};
     }
   }
 
